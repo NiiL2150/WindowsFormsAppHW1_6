fb664ff2b2b5cb91f806c418f143f3bb75682054
NiiL2150 <nail.shamsudinov@gmail.com>
2021-06-22 19:22:21 +0400

    Add project files.

---
+++ WindowsFormsAppHW1_6/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppHW1_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = (DateTime.Parse(textBox1.Text)).DayOfWeek.ToString();
+            }
+            catch (Exception)
+            {
+                label1.Text = "Invalid date";
+            }
+        }
+    }
+}
